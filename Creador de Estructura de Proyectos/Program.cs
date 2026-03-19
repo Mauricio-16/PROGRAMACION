445068b359647eaeb443fa549c1b5ef99588cbe6
Mauricio-16 <andresymelannie3@gmail.com>
2026-03-19 14:57:12 -0500

    new

---
+++ Creador de Estructura de Proyectos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creador_de_Estructura_de_Proyectos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nombre del proyecto: ");
+            string nombreproyecto = Console.ReadLine();
+
+            Directory.CreateDirectory(nombreproyecto);
+
+            Directory.CreateDirectory(nombreproyecto + "/Documentos");
+            Directory.CreateDirectory(nombreproyecto + "/Imagenes");
+            Directory.CreateDirectory(nombreproyecto + "/Codigo");
+
+            Console.Write("Ingrese una breve descripcion del proyecto: ");
+            string descripcion = Console.ReadLine();
+
+            string rutaArchivo = Path.Combine(nombreproyecto, "documentos", "readme.txt");
+
+            File.WriteAllText(rutaArchivo, descripcion);
+
+            Console.WriteLine("\nUbicación completa:");
+            Console.WriteLine(Path.GetFullPath(nombreproyecto));
+        }
+    }
+}
